ac92f7120a13b6e8c3dc757daebc4a7769c11f03
DAShumilin <92357174+DAShumilin@users.noreply.github.com>
2022-02-13 20:33:41 +0300

    File

---
+++ ConsoleApplication/ConsoleApplication.cpp
@@ -0,0 +1,30 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main() {
+
+    FILE* file;
+    char name[] = "1.txt";
+    int y = 0;
+
+    if ((file = fopen(name, "r")) == NULL){
+
+        printf("Error");
+        return 0;
+    }
+    
+    else {
+
+        while (fscanf(file, "%*s") != EOF) {
+
+            ++y;
+        }
+    }
+
+    printf("%d", y);
+
+    fclose(file);
+
+    return 0;
+}
